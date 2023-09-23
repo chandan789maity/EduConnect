e7b80cdcd0f05a3be73d551ca5fac66c0f98f409
Basu-Dev-Ghosh <basu1735@gmail.com>
2023-09-23 08:47:54 +0530

    some updates

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ import AboutUs from "./page/aboutUs";
 // import ProtectedRoute from "./page/protected";
 const queryClient = new QueryClient();
 
-// export const server = "https://educonnect-server.onrender.com/api/v1/";  //Hosted server
-export const server = "http://localhost:4000/api/v1/";
+export const server = "https://educonnect-server.onrender.com/api/v1/";  //Hosted server
+// export const server = "http://localhost:4000/api/v1/";
 
 function App() {
   return (
